b8cfa76ebf3e71a284a1600bf63db66363e6914b
reo4188 <chaehunjeong@gmail.com>
2019-01-18 16:34:41 +0900

    spring boot project 생성
    초기 프로젝트 작업 시작

---
+++ my-j4family/src/main/java/com/j4family/MyJ4familyApplication.java
@@ -0,0 +1,14 @@
+package com.j4family;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJ4familyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJ4familyApplication.class, args);
+	}
+
+}
+
